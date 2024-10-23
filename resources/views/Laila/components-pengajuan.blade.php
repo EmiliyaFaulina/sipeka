d3968893f4f43abae688210ca7f6403d336728a5
unknown <ylala0216@gmail.com>
2024-10-23 15:18:18 +0700

    lailaupdate2310

--- resources/views/Laila/components-pengajuan.blade.php
+++ resources/views/Laila/components-pengajuan.blade.php
@@ -18,16 +18,16 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="{{ asset('')}}assets2/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin1
@@ -209,16 +209,16 @@
         <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="tables-project.html">
-              <i class="bi bi-circle"></i><span>Project</span>
-            </a>
-          </li>
-          </li>
-            <a href="tables-progres.html">
-              <i class="bi bi-circle"></i><span>Progres</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
+                <i class="bi bi-circle"></i><span>Project</span>
+              </a>
+            </li>
+            </li>
+              <a href="tables-progres.html">
+                <i class="bi bi-circle"></i><span>Progres</span>
+              </a>
+            </li>
+          </ul>
+        </li><!-- End Tables Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
@@ -407,17 +407,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.js"></script>
-  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/chart.js/chart.umd.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/echarts/echarts.min.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/quill/quill.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/tinymce/tinymce.min.js"></script>
+  <script src="{{ asset('')}}assets2/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{ asset('')}}assets2/js/main.js"></script>
 
 </body>
 
---
+++ resources/views/Laila/forms/pengajuan.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app')
+
+@section('content')
+<main class="main">
+<h1><section class="section dashboard">
+    <div class="row">
+
+      <!-- Left side columns -->
+      <div class="col-lg-8">
+        <div class="row">
+
+          <div class="row">
+            <!-- Pengguna Card -->
+            <div class="col-xxl-6 col-md-6 mb-4"> <!-- Mengatur menjadi 6 kolom untuk 2 kartu dalam satu baris -->
+              <div class="card info-card sales-card h-100"> <!-- Menambahkan h-100 untuk tinggi yang sama -->
+          
+                <div class="filter"></div>
+          
+                <div class="card-body">
+                  <h5 class="card-title">Pengguna</h5>
+          
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <i class="bi bi-people"></i>
+                    </div>
+                    <div class="ps-3">
+                      <h6>12</h6>
+                      <span class="text-success small pt-1 fw-bold">2</span>
+                      <span class="text-muted small pt-2 ps-1">Pengguna Baru</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- End Pengguna Card -->
+          
+            <!-- Pengaduan Card -->
+            <div class="col-xxl-6 col-md-6 mb-4"> <!-- Mengatur menjadi 6 kolom untuk 2 kartu dalam satu baris -->
+              <div class="card info-card revenue-card h-100"> <!-- Menambahkan h-100 untuk tinggi yang sama -->
+          
+                <div class="filter"></div>
+          
+                <div class="card-body">
+                  <h5 class="card-title">Pengaduan</h5>
+          
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <i class="bi bi-envelope"></i>
+                    </div>
+                    <div class="ps-3">
+                      <h6>14</h6>
+                      <span class="text-success small pt-1 fw-bold">4</span>
+                      <span class="text-muted small pt-2 ps-1">Pengaduan Baru</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- End Pengaduan Card -->
+          
+            <!-- Projek Card -->
+            <div class="col-xxl-6 col-md-6 mb-4"> <!-- Mengatur menjadi 6 kolom untuk 2 kartu dalam satu baris -->
+              <div class="card info-card customers-card h-100"> <!-- Menambahkan h-100 untuk tinggi yang sama -->
+          
+                <div class="filter"></div>
+          
+                <div class="card-body">
+                  <h5 class="card-title">Projek</h5>
+          
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <i class="bi bi-files"></i>
+                    </div>
+                    <div class="ps-3">
+                      <h6>5</h6>
+                      <span class="text-danger small pt-1 fw-bold">4</span>
+                      <span class="text-muted small pt-2 ps-1">Projek Baru</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- End Projek Card -->
+          
+            <!-- Laporan Card -->
+            <div class="col-xxl-6 col-md-6 mb-4"> <!-- Mengatur menjadi 6 kolom untuk 2 kartu dalam satu baris -->
+              <div class="card info-card customers-card h-100"> <!-- Menambahkan h-100 untuk tinggi yang sama -->
+          
+                <div class="filter"></div>
+          
+                <div class="card-body">
+                  <h5 class="card-title">Laporan</h5>
+          
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <i class="bi bi-folder"></i>
+                    </div>
+                    <div class="ps-3">
+                      <h6>14</h6>
+                      <span class="text-danger small pt-1 fw-bold">4</span>
+                      <span class="text-muted small pt-2 ps-1">Laporan Baru</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- End Laporan Card -->
+          </div>
+          
+       
+
+    </div>
+  </section>
+</h1>
+</main>
+
+@endsection
