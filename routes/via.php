614cf0be1f3934c2f92a25398158188a66f4a42b
unknown <vianeyla917@gmail.com>
2024-10-24 15:34:22 +0800

    update

--- routes/via.php
+++ routes/via.php
@@ -15,7 +15,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/via', function () {  return view('welcome');});
 
-Route::get('/sk', function () { return view('via.index');});
-Route::get('/Profil', function () { return view('via.users-profile');});
+Route::get('/via/Persadistik', function () { return view('via.index');});
+
+Route::get('/via/Profil', function () { return view('via.users-profile');});
+
+Route::get('/via/Pengajuan', function () { return view('via.components-pengajuan');});
+
+Route::get('/via/Pengaduan', function () { return view('via.components-pengaduan');});
+
+Route::get('/via/Projek', function () { return view('via.tables-project');});
+
+Route::get('/via/Progres', function () { return view('via.tables-progres');});
+
+Route::get('/via/LaporanDiterima', function () { return view('via.charts-diterima');});
+
+Route::get('/via/LaporanDitolak', function () { return view('via.charts-ditolak');});
+
+Route::get('/vian/LogOut', function () { return view('via.pages-logout');});
+
 
 
