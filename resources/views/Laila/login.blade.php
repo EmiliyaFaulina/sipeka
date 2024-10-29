347b6c08c0b0e494e82700f082a015313e2c55db
unknown <ylala0216@gmail.com>
2024-10-29 10:46:23 +0700

    ylala

--- resources/views/Laila/login.blade.php
+++ resources/views/Laila/login.blade.php
@@ -88,7 +88,7 @@
                       </div>
                     </div>
                     <div class="col-12">
-                      <a href="{{url('home')}}"class="btn btn-primary w-100" type="submit">Login</a>
+                      <a href="{{url('lai')}}"class="btn btn-primary w-100" type="submit">Login</a>
                     </div>
                     <div class="col-12">
                       <p class="small mb-0">Tidak Punya Akun? <a href="pages-register.html">Buat Akun</a></p>
