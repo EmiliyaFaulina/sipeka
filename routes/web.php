545586c0acad8ce9f2c51e24feab183bdc9cb6da
GirlTenten <at3846004@gmail.com>
2024-10-31 11:23:55 +0800

    update3110

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-    
-});
+Route::get('/', function () { return view('home');});
+
+Route::get('/register', function () { return view('emiliya.register');});
+
+Route::get('/login', function () { return view('laila.login');});
+
+Route::get('/home', function () { return view('jannah.index');});
+
+Route::get('/egove', function () { return view('jannah.index');});
+
+Route::get('/inkop', function () { return view('jannah.index');});
+
+Route::get('/persandistik', function () { return view('jannah.index');});
+
+Route::get('/kesekretariatan', function () { return view('jannah.index');});
+
+Route::get('/emiliya', function () {return view('welcome');});
+
+Route::get('/pengajuan', function () {return view('emiliya');});
+
+Route::get('/project', function () { return view('emiliya.tables-project');});
+
+Route::get('/laporan', function () { return view('laila.charts-diterima');});
+
+Route::get('/profil', function () { return view('laila.user-profil');});
+
+Route::get('/logout', function () { return view('laila.login');});
